83557083943125c259310e64409016a7e0940a4e
preksatic <79179032+preksatic@users.noreply.github.com>
2025-01-01 22:23:59 +0900

    .

--- h2o_hf/utils_hh/modify_llama.py
+++ h2o_hf/utils_hh/modify_llama.py
@@ -111,7 +111,8 @@ class LlamaAttention_heavy_hitter(nn.Module):
 
 
         if self.attention_masks_next is not None:
-            self.attention_masks_next[:, :, :, :4] = 1
+            if self.random_small_cache:
+                self.attention_masks_next[:, :, :, :4] = 1
             attn_weights = attn_weights * self.attention_masks_next + (1 - self.attention_masks_next) * torch.finfo(attn_weights.dtype).min
 
         # upcast attention to fp32
